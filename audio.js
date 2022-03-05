d8484039fea0bbcb40e4ec790bc1d9361678e494
riteshsinghh-coder <93989204+riteshsinghh-coder@users.noreply.github.com>
2022-03-05 09:39:10 +0530

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,17 @@
+var i=0;
+setInterval(()=>{
+    let audio=new Audio('sound.mp3');
+    audio.loop=true;
+    audio.play();
+    i++;
+    console.log(i)
+    
+
+},9000);
+let audio=new Audio('sound.mp3');
+    audio.loop=true;
+    audio.play();
+
+
+
+
